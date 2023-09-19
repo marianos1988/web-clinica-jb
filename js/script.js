e8b61eb5917a6b22ac7a22066297f9924b06ffff
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2023-09-19 17:30:26 -0300

    Agregar fullscreen en videos

--- js/script.js
+++ js/script.js
@@ -308,22 +308,22 @@ const mostrarServiciosResponsive = () => {
       })
       if(item.id === `video-1`) {
         document.querySelector(`.${item.id}`).innerHTML = `
-        <iframe src="https://drive.google.com/file/d/1_pBj1tr9cuFW2jgLmIzRwa73nQoLeIYx/preview" width="100%" height="100%" allow="autoplay"></iframe>
+        <iframe src="https://drive.google.com/file/d/1_pBj1tr9cuFW2jgLmIzRwa73nQoLeIYx/preview" width="100%" height="100%" allow="autoplay" allowfullscreen></iframe>
         `;
       }
       else if(item.id === `video-2`) {
         document.querySelector(`.${item.id}`).innerHTML = `
-        <iframe src="https://drive.google.com/file/d/1Rm3VRL-nv3PdWFzhM0W-NDa7xm1GX4do/preview" width="100%" height="100%" allow="autoplay"></iframe>
+        <iframe src="https://drive.google.com/file/d/1Rm3VRL-nv3PdWFzhM0W-NDa7xm1GX4do/preview" width="100%" height="100%" allow="autoplay" allowfullscreen></iframe>
         `;
       }
       else if(item.id === `video-3`) {
         document.querySelector(`.${item.id}`).innerHTML = `
-        <iframe src="https://drive.google.com/file/d/1SZRJh5ER9nzj2-oKoxy2ktEWOR4iYP8C/preview" width="100%" height="100%" allow="autoplay"></iframe>
+        <iframe src="https://drive.google.com/file/d/1SZRJh5ER9nzj2-oKoxy2ktEWOR4iYP8C/preview" width="100%" height="100%" allow="autoplay" allowfullscreen></iframe>
         `;
       }
       else if(item.id === `video-4`) {
         document.querySelector(`.${item.id}`).innerHTML = `
-        <iframe src="https://drive.google.com/file/d/1c0gHfgJUKUNfskdIRvMrBATQiPdx1sP_/preview" width="100%" height="100%" allow="autoplay"></iframe>
+        <iframe src="https://drive.google.com/file/d/1c0gHfgJUKUNfskdIRvMrBATQiPdx1sP_/preview" width="100%" height="100%" allow="autoplay" allowfullscreen></iframe>
         `;
       }
     })
