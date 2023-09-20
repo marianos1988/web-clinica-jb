d8d4327ef54d2617a81a10c68cb50d358a012dfc
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2023-09-20 14:31:24 -0300

    TRaer videos desde url canva

--- js/responsive-servicios.js
+++ js/responsive-servicios.js
@@ -14,28 +14,51 @@ items.forEach((item) => {
 		})
 		if(item.id === `video-1`) {
 
+			// document.querySelector(`.${item.id}`).innerHTML = `
+			// <video class="vid-servicios ${item.id}" src="./videos/${item.id}.mp4" autoplay></video>
+			// `;
 			document.querySelector(`.${item.id}`).innerHTML = `
-			<video class="vid-servicios ${item.id}" src="./videos/${item.id}.mp4" autoplay></video>
+			 <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+			   src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFvAZFVRIA&#x2F;watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+			 </iframe>
 			`;
 			
 		}
 		else if(item.id === `video-2`) {
+			// document.querySelector(`.${item.id}`).innerHTML = `
+			// <video class="vid-servicios ${item.id}" src="./videos/${item.id}.mp4" autoplay></video>
+			// `;
 			document.querySelector(`.${item.id}`).innerHTML = `
-			<video class="vid-servicios ${item.id}" src="./videos/${item.id}.mp4" autoplay></video>
+			 <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+			   src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFvAd2D4aQ&#x2F;watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+			 </iframe>
 			`;
 
-			
 		}
 		else if(item.id === `video-3`) {
+			// document.querySelector(`.${item.id}`).innerHTML = `
+			// <video class="vid-servicios ${item.id}" src="./videos/${item.id}.mp4" autoplay></video>
+			// `;
 			document.querySelector(`.${item.id}`).innerHTML = `
-			<video class="vid-servicios ${item.id}" src="./videos/${item.id}.mp4" autoplay></video>
+			 <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+			   src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFvAeLlqiU&#x2F;watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+			 </iframe>
 			`;
 
 		}
 		else if(item.id === `video-4`) {
+			// document.querySelector(`.${item.id}`).innerHTML = `
+			// <video class="vid-servicios ${item.id}" src="./videos/${item.id}.mp4" autoplay></video>
+			// `;
 			document.querySelector(`.${item.id}`).innerHTML = `
-			<video class="vid-servicios ${item.id}" src="./videos/${item.id}.mp4" autoplay></video>
+			 <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+			   src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFvAaNFzYI&#x2F;watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+			 </iframe>
 			`;
+
+
+
+
 		}
 
 
